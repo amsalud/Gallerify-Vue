58b1814ed34234149e09f813da4a260e013416ad
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2018-10-17 21:56:41 -0400

    Server init

---
+++ server.js
@@ -0,0 +1,17 @@
+const { ApolloServer, gql } = require('apollo-server')
+
+const typeDefs = gql`
+  type Todo {
+    task: String
+    completed: Boolean
+  }
+  type Query {
+    getTodos: [Todo]
+  }
+`
+
+const server = new ApolloServer({ typeDefs })
+
+server.listen().then(({ url }) => {
+  console.log(`Server listening on ${url}`)
+})
