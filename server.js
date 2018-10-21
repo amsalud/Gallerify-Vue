a557a2842417c6c813c5d903a1c79a9cfebd1548
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2018-10-20 21:08:56 -0400

    Add comments for future reference purposes

--- server.js
+++ server.js
@@ -1,16 +1,19 @@
 const { ApolloServer, gql } = require('apollo-server');
 const mongoose = require('mongoose');
-
 const fs = require('fs');
 const path = require('path');
 
+// Import typeDefs and Resolvers
 const filePath = path.join(__dirname, 'typeDefs.gql');
 const typeDefs = fs.readFileSync(filePath, 'utf-8');
 const resolvers = require('./resolvers');
-require('dotenv').config({ path: 'variables.env' });
 
+// Import Environment Variables and Mongoose Models
+require('dotenv').config({ path: 'variables.env' });
 const User = require('./models/User');
 const Post = require('./models/Post');
+
+// Connect to MongoDB MLab Database
 mongoose
   .connect(
     process.env.MONGO_URI,
@@ -19,6 +22,7 @@ mongoose
   .then(() => console.log('Successfully Connected to MongoDB'))
   .catch(err => console.error(err));
 
+// Create Apollo GraphQl Server using TypeDefs, resolvers, and context object
 const server = new ApolloServer({
   typeDefs,
   resolvers,
