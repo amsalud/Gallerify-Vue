e628c153f3d6de12baede182f3ccf7cede013d43
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2018-10-20 22:25:53 -0400

    Apply theme

--- ui/src/plugins/vuetify.js
+++ ui/src/plugins/vuetify.js
@@ -1,7 +1,16 @@
-import Vue from 'vue'
-import Vuetify from 'vuetify/lib'
-import 'vuetify/src/stylus/app.styl'
+import Vue from 'vue';
+import Vuetify from 'vuetify/lib';
+import 'vuetify/src/stylus/app.styl';
 
 Vue.use(Vuetify, {
   iconfont: 'md',
-})
+  theme: {
+    primary: '#1E88E5',
+    secondary: '#64B5F6',
+    accent: '#29B6F6',
+    error: '#F4511E',
+    warning: '#FF8F00',
+    info: '#455A64',
+    success: '#689F38'
+  }
+});
