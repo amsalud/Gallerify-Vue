ea8618a74d9ead6a90e2b5b306bb999b8166636e
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2018-10-21 22:06:32 -0400

    Implement ApolloQuery to handle data results

--- ui/src/components/Home.vue
+++ ui/src/components/Home.vue
@@ -1,14 +1,19 @@
 <template>
   <v-container>
     <h1>Home</h1>
-    <div v-if="$apollo.loading">Loading...</div>
-    <ul v-else v-for="post in getPosts" :key="post._id">
-      <li>
-        {{post.title}}
-        {{post.imageUrl}}
-        {{post.description}}
-      </li>
-    </ul>
+    <ApolloQuery :query="getPostsQuery">
+      <template slot-scope="{result: {loading, error, data}}">
+        <div v-if="loading">Loading...</div>
+        <div v-else-if="error">Error {{error.message}}</div>
+        <ul v-else-if="data" v-for="post in data.getPosts" :key="post._id">
+          <li>
+            {{post.title}}
+            {{post.imageUrl}}
+            {{post.description}}
+          </li>
+        </ul>
+      </template>
+    </ApolloQuery>
   </v-container>
 </template>
 
@@ -17,29 +22,16 @@ import { gql } from 'apollo-boost';
 export default {
   name: 'home',
   data: () => ({
-    posts: []
-  }),
-  apollo: {
-    getPosts: {
-      query: gql`
-        query {
-          getPosts {
-            _id
-            title
-            imageUrl
-            description
-          }
+    getPostsQuery: gql`
+      query {
+        getPosts {
+          _id
+          title
+          imageUrl
+          description
         }
-      `,
-      result({ data, loading }) {
-        if (!loading) {
-          this.posts = data.getPosts;
-        }
-      },
-      error(err) {
-        console.error('[ERROR]', err);
       }
-    }
-  }
+    `
+  })
 };
 </script>
