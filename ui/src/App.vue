e91777d2724910c9f86ee43282026d67961c25b6
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2018-10-21 20:37:34 -0400

    Apply animations when switching routes

--- ui/src/App.vue
+++ ui/src/App.vue
@@ -44,7 +44,9 @@
       <!-- App Content -->
       <main>
         <v-container class="mt-4">
-          <router-view />
+          <transition name="fade">
+            <router-view />
+          </transition>
         </v-container>
       </main>
     </v-app>
@@ -80,3 +82,19 @@ export default {
   }
 };
 </script>
+<style>
+.fade-enter-active,
+.fade-leave-active {
+  transition-property: opacity;
+  transition-duration: 0.25s;
+}
+.fade-enter-active {
+  transition-delay: 0.25s;
+}
+.fade-enter,
+fade-leave-active {
+  opacity: 0;
+}
+</style>
+
+
