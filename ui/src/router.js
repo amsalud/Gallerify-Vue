42431ec4a8f9828802436e4c1458669374b75aa7
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2018-10-21 20:34:38 -0400

    Add new routes

--- ui/src/router.js
+++ ui/src/router.js
@@ -2,6 +2,12 @@ import Vue from 'vue';
 import Router from 'vue-router';
 import Home from './components/Home.vue';
 
+import Posts from './components/posts/Posts.vue';
+import AddPost from './components/posts/AddPost.vue';
+import Signin from './components/auth/Signin.vue';
+import Signup from './components/auth/Signup.vue';
+import Profile from './components/auth/Profile.vue';
+
 Vue.use(Router);
 
 export default new Router({
@@ -12,6 +18,31 @@ export default new Router({
       path: '/',
       name: 'home',
       component: Home
+    },
+    {
+      path: '/posts',
+      name: 'posts',
+      component: Posts
+    },
+    {
+      path: '/post/add',
+      name: 'addpost',
+      component: AddPost
+    },
+    {
+      path: '/profile',
+      name: 'profile',
+      component: Profile
+    },
+    {
+      path: '/signin',
+      name: 'signin',
+      component: Signin
+    },
+    {
+      path: '/signup',
+      name: 'signup',
+      component: Signup
     }
   ]
 });
