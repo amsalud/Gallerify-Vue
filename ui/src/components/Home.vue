b511d187fbcfe30d0fa95bccb77b07663d0b90f7
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2018-10-21 22:19:40 -0400

    Add image carousel in homepage

--- ui/src/components/Home.vue
+++ ui/src/components/Home.vue
@@ -1,17 +1,16 @@
 <template>
-  <v-container>
-    <h1>Home</h1>
+  <v-container text-xs-center>
     <ApolloQuery :query="getPostsQuery">
       <template slot-scope="{result: {loading, error, data}}">
         <div v-if="loading">Loading...</div>
         <div v-else-if="error">Error {{error.message}}</div>
-        <ul v-else-if="data" v-for="post in data.getPosts" :key="post._id">
-          <li>
-            {{post.title}}
-            {{post.imageUrl}}
-            {{post.description}}
-          </li>
-        </ul>
+        <v-flex v-else-if="data" xs12>
+          <v-carousel v-bind="{'cycle': true}" interval="3000">
+            <v-carousel-item v-for="post in data.getPosts" :key="post._id" :src="post.imageUrl">
+              <h1 id="carousel__title">{{post.title}}</h1>
+            </v-carousel-item>
+          </v-carousel>
+        </v-flex>
       </template>
     </ApolloQuery>
   </v-container>
@@ -35,3 +34,18 @@ export default {
   })
 };
 </script>
+
+<style>
+#carousel__title {
+  position: absolute;
+  background-color: rgba(0, 0, 0, 0.5);
+  color: white;
+  border-radius: 5px 5px 0 0;
+  padding: 0.5m;
+  margin: 0 auto;
+  bottom: 50px;
+  left: 0;
+  right: 0;
+}
+</style>
+
