6ac771c47d778c14c81f97fff0cd93a2ef186490
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2018-10-23 22:05:46 -0400

    Add conditional side nav items

--- ui/src/App.vue
+++ ui/src/App.vue
@@ -54,25 +54,43 @@
 </template>
 
 <script>
+import { mapGetters } from 'vuex';
 export default {
   name: 'App',
   data: () => ({
     sideNavVisible: false
   }),
   computed: {
+    ...mapGetters(['user']),
     horizontalNavItems() {
-      return [
+      let items = [
         { icon: 'chat', title: 'Posts', link: '/posts' },
         { icon: 'lock_open', title: 'Sign In', link: '/signin' },
         { icon: 'create', title: 'Sign Up', link: 'signup' }
       ];
+
+      if (this.user) {
+        items = [{ icon: 'chat', title: 'Posts', link: '/posts' }];
+      }
+
+      return items;
     },
     sideNavItems() {
-      return [
+      let items = [
         { icon: 'chat', title: 'Posts', link: '/posts' },
         { icon: 'lock_open', title: 'Sign In', link: '/signin' },
         { icon: 'create', title: 'Sign Up', link: 'signup' }
       ];
+
+      if (this.user) {
+        items = [
+          { icon: 'chat', title: 'Posts', link: '/posts' },
+          { icon: 'stars', title: 'Create a Post', link: '/post/add' },
+          { icon: 'account_box', title: 'Profile', link: '/profile' }
+        ];
+      }
+
+      return items;
     }
   },
   methods: {
