a81fd3d2841adae97e61e473bf99951da9589c53
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2018-10-24 20:58:47 -0400

    Add Form alert component

---
+++ ui/src/components/shared/FormAlert.vue
@@ -0,0 +1,11 @@
+<template>
+    <v-alert type="error" v-model="alert" :value="true" dismissible>
+        <h3>{{message}}</h3>
+    </v-alert>
+</template>
+
+<script>
+export default {
+  props: ['message ']
+};
+</script>
