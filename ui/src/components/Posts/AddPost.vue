72a79b464d72b4493dfdbc2b01babd266f29614c
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2019-03-19 21:20:37 -0400

    Add post form layout

--- ui/src/components/Posts/AddPost.vue
+++ ui/src/components/Posts/AddPost.vue
@@ -1,11 +1,141 @@
 <template>
-    <v-container>
-        <h1>Add Post</h1>
-    </v-container>
+  <v-container
+    text-xs-center
+    mt-5
+    pt-5
+  >
+
+    <!-- Signup Form -->
+    <v-layout
+      row
+      wrap
+    >
+      <v-flex
+        xs-12
+        sm6
+        offset-sm3
+      >
+        <v-card>
+          <v-layout
+            row
+            wrap
+          >
+            <v-flex
+              xs12
+              sm6
+              offset-sm3
+              mt-3
+            >
+              <h1>Add a post</h1>
+            </v-flex>
+          </v-layout>
+          <!-- Add Post Form -->
+          <v-container>
+            <v-form
+              v-model="isFormValid"
+              lazy-validation
+              ref="form"
+              @submit.prevent="addPost"
+            >
+              <!-- Title Input -->
+              <v-layout row>
+                <v-flex xs12>
+                  <v-text-field
+                    :rules="titleRules"
+                    v-model="title"
+                    label="Post Title"
+                    type="text"
+                    required
+                  ></v-text-field>
+                </v-flex>
+              </v-layout>
+
+              <!-- Image Url -->
+              <v-layout row>
+                <v-flex xs12>
+                  <v-text-field
+                    :rules="imageRules"
+                    v-model="imageUrl"
+                    label="Image Url"
+                    type="text"
+                    required
+                  ></v-text-field>
+                </v-flex>
+              </v-layout>
+
+              <!-- Image Preview -->
+              <v-layout row>
+                <v-flex xs12>
+                  <img
+                    :src="imageUrl"
+                    height="300px"
+                  >
+                </v-flex>
+              </v-layout>
+
+              <!-- Categories Select -->
+              <v-layout row>
+                <v-flex xs12>
+                  <v-select
+                    v-model="categories"
+                    :items="['Art', 'Education', 'Travel', 'Photography', 'Technology']"
+                    multiple
+                    label="Categories"
+                  ></v-select>
+                </v-flex>
+              </v-layout>
+
+              <!-- Description -->
+              <v-layout row>
+                <v-flex xs12>
+                  <v-textarea
+                    :rules="descriptionRules"
+                    v-model="description"
+                    label="Description"
+                    type="text"
+                    required
+                  ></v-textarea>
+                </v-flex>
+
+              </v-layout>
+
+              <v-layout row>
+                <v-flex xs12>
+                  <v-btn
+                    :loading="loading"
+                    color="accent"
+                    type="submit"
+                    :disabled="!isFormValid || loading"
+                  >Create
+                    <span
+                      slot="loader"
+                      class="custom-loader"
+                    >
+                      <v-icon light>cached</v-icon>
+                    </span>
+                  </v-btn>
+                </v-flex>
+              </v-layout>
+
+            </v-form>
+          </v-container>
+        </v-card>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
 
 <script>
 export default {
-  name: 'AddPost'
+  name: "AddPost",
+  data() {
+    return {
+      isFormValid: true,
+      title: "",
+      imageUrl: "",
+      categories: [],
+      description: ""
+    };
+  }
 };
 </script>
