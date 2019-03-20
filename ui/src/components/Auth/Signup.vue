557e5ace6e59812914ec960e0d23b9489224506f
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2019-03-19 20:07:42 -0400

    Add sign up form layout

--- ui/src/components/Auth/Signup.vue
+++ ui/src/components/Auth/Signup.vue
@@ -1,11 +1,143 @@
 <template>
-    <v-container>
-        <h1>Sign up</h1>
-    </v-container>
+  <v-container
+    text-xs-center
+    mt-5
+    pt-5
+  >
+    <!-- Error Alert -->
+    <v-layout
+      row
+      wrap
+      v-if="error"
+    >
+      <v-flex
+        xs12
+        sm6
+        offset-sm3
+      >
+        <form-alert :message="error.message"></form-alert>
+      </v-flex>
+    </v-layout>
+    <!-- Signup Form -->
+    <v-layout
+      row
+      wrap
+    >
+      <v-flex
+        xs-12
+        sm6
+        offset-sm3
+      >
+        <v-card>
+          <v-layout
+            row
+            wrap
+          >
+            <v-flex
+              xs12
+              sm6
+              offset-sm3
+              mt-3
+            >
+              <h1>Get Started Here</h1>
+            </v-flex>
+          </v-layout>
+          <v-container>
+            <v-form
+              v-model="isFormValid"
+              lazy-validation
+              ref="form"
+              @submit.prevent="signupUser"
+            >
+              <v-layout row>
+                <v-flex xs12>
+                  <v-text-field
+                    :rules="usernameRules"
+                    v-model="username"
+                    prepend-icon="face"
+                    label="Username"
+                    type="text"
+                    required
+                  ></v-text-field>
+                </v-flex>
+              </v-layout>
+              <v-layout row>
+                <v-flex xs12>
+                  <v-text-field
+                    :rules="emailRules"
+                    v-model="email"
+                    prepend-icon="email"
+                    label="Email"
+                    type="email"
+                    required
+                  ></v-text-field>
+                </v-flex>
+              </v-layout>
+              <v-layout row>
+                <v-flex xs12>
+                  <v-text-field
+                    :rules="passwordRules"
+                    v-model="password"
+                    prepend-icon="extension"
+                    label="Password"
+                    type="password"
+                    required
+                  ></v-text-field>
+                </v-flex>
+              </v-layout>
+              <v-layout row>
+                <v-flex xs12>
+                  <v-text-field
+                    :rules="passwordRules"
+                    v-model="passwordConfirmation"
+                    prepend-icon="gavel"
+                    label="Confirm Password"
+                    type="password"
+                    required
+                  ></v-text-field>
+                </v-flex>
+              </v-layout>
+              <v-layout row>
+                <v-flex xs12>
+                  <v-btn
+                    :loading="loading"
+                    color="accent"
+                    type="submit"
+                    :disabled="!isFormValid"
+                  >Sign Up
+                    <span
+                      slot="loader"
+                      class="custom-loader"
+                    >
+                      <v-icon light>cached</v-icon>
+                    </span>
+                  </v-btn>
+                  <p>Already have an account?
+                    <router-link to="/signin">Sign In </router-link>
+                  </p>
+                </v-flex>
+              </v-layout>
+
+            </v-form>
+          </v-container>
+        </v-card>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
 
 <script>
+import { mapGetters } from "vuex";
 export default {
-  name: 'Signup'
+  name: "Signup",
+  data: () => ({
+    isFormValid: true
+  }),
+  computed: {
+    ...mapGetters(["error"])
+  },
+  methods: {
+    handleSignupUser() {}
+  }
 };
 </script>
