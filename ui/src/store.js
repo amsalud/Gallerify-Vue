55d8ef3839d424ce35c0ecbb19f8ad167163b11f
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2019-03-19 20:54:02 -0400

    Add signupUser action method

--- ui/src/store.js
+++ ui/src/store.js
@@ -3,7 +3,12 @@ import Vuex from 'vuex';
 import router from './router';
 
 import { defaultClient as apolloClient } from './main';
-import { GET_POSTS, SIGNIN_USER, GET_CURRENT_USER } from './queries';
+import {
+  GET_POSTS,
+  SIGNIN_USER,
+  SIGNUP_USER,
+  GET_CURRENT_USER
+} from './queries';
 
 Vue.use(Vuex);
 
@@ -28,7 +33,7 @@ export default new Vuex.Store({
     clearUser: state => (state.user = null),
     setError: (state, payload) => (state.error = payload),
     clearError: state => (state.error = null),
-    setAuthError: (state, payload) =>{
+    setAuthError: (state, payload) => {
       state.authError = payload;
     }
   },
@@ -76,8 +81,7 @@ export default new Vuex.Store({
     signinUser: ({ commit }, payload) => {
       commit('clearError');
       commit('setLoading', true);
-      // Prevent errors if token is malformed or invalid
-      localStorage.setItem('token', '');
+
       apolloClient
         .mutate({
           mutation: SIGNIN_USER,
@@ -86,6 +90,27 @@ export default new Vuex.Store({
         .then(({ data }) => {
           commit('setLoading', false);
           localStorage.setItem('token', data.signinUser.token);
+          router.go();
+        })
+        .catch(err => {
+          commit('setLoading', false);
+          commit('setError', err);
+          console.error(err);
+        });
+    },
+    signupUser: ({ commit }, payload) => {
+      commit('clearError');
+      commit('setLoading', true);
+
+      apolloClient
+        .mutate({
+          mutation: SIGNUP_USER,
+          variables: payload
+        })
+        .then(({ data }) => {
+          commit('setLoading', false);
+          localStorage.setItem('token', data.signupUser.token);
+          // Make sure created methid is executed in main.js (Run getCurrentUser) and reaload the page
           router.go();
         })
         .catch(err => {
