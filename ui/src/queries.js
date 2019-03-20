6ab90b2dba70870d4d237d8ea3d0219e65d237e1
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2019-03-20 04:34:55 -0400

    Create ADD_POST mutation

--- ui/src/queries.js
+++ ui/src/queries.js
@@ -46,3 +46,29 @@ export const SIGNUP_USER = gql`
     }
   }
 `;
+
+/* Post Mutations */
+export const ADD_POST = gql`
+  mutation(
+    $title: String!
+    $imageUrl: String!
+    $categories: [String]!
+    $description: String!
+    $creatorId: ID!
+  ) {
+    addPost(
+      title: $title
+      imageUrl: $imageUrl
+      categories: $categories
+      description: $description
+      creatorId: $creatorId
+    ) {
+      _id
+      title
+      imageUrl
+      categories
+      description
+      createdDate
+    }
+  }
+`;
