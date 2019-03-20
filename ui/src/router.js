6557995a1edd89812f6b3d5bd142dad67166ce17
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2019-03-20 11:37:06 -0400

    Enable props in post id route

--- ui/src/router.js
+++ ui/src/router.js
@@ -30,7 +30,8 @@ export default new Router({
     {
       path: '/posts/:postId',
       name: 'post',
-      component: Post
+      component: Post,
+      props: true
     },
     {
       path: '/post/add',
