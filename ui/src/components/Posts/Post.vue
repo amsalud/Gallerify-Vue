0f75fb7982f5fcf3d256f8a5ab3555d4f123d494
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2019-03-20 12:26:39 -0400

    Add Post page layout

--- ui/src/components/Posts/Post.vue
+++ ui/src/components/Posts/Post.vue
@@ -1,15 +1,81 @@
 <template>
-  <v-container v-if="getPost">
-    <h1>{{getPost.title}}</h1>
+  <v-container
+    v-if="getPost"
+    class="mt-3"
+    flexbox
+    center
+  >
+    <!-- Post Card -->
+    <v-layout
+      row
+      wrap
+    >
+      <v-flex xs12>
+        <v-card hover>
+          <v-card-title>
+            <h1>{{getPost.title}}</h1>
+            <v-btn
+              large
+              icon
+              v-if="user"
+            >
+              <v-icon
+                large
+                color="grey"
+              >favorite</v-icon>
+            </v-btn>
+            <h3 class="ml-3 font-weight-thin">{{getPost.likes}} LIKES</h3>
+            <v-spacer></v-spacer>
+            <v-icon
+              color="info"
+              large
+            >arrow_back</v-icon>
+          </v-card-title>
+          <v-tooltip right>
+            <span>Click to enlarge image</span>
+            <v-img
+              slot="activator"
+              :src="getPost.imageUrl"
+              id="post__image"
+            ></v-img>
+          </v-tooltip>
+          <!-- Post Image Modal -->
+          <v-dialog v-model="dialog">
+            <v-card>
+              <v-img
+                :src="getPost.imageUrl"
+                height="500px"
+              ></v-img>
+            </v-card>
+          </v-dialog>
+
+          <v-card-text>
+            <span
+              v-for="(category, index) in getPost.categories"
+              :key="index"
+            >
+                <v-chip class="mb-3" color="accent" text-color="white">{{category}}</v-chip>
+            </span>
+            <h3>{{getPost.description}}</h3>
+          </v-card-text>
+        </v-card>
+      </v-flex>
+    </v-layout>
   </v-container>
 </template>
 
 <script>
+import { mapGetters } from "vuex";
 import { GET_POST } from "../../queries";
 
 export default {
   name: "Post",
   props: ["postId"],
+  data() {
+    return {
+      dialog: false
+    };
+  },
   apollo: {
     getPost: {
       query: GET_POST,
@@ -19,7 +85,17 @@ export default {
         };
       }
     }
+  },
+  computed: {
+    ...mapGetters(["user"])
   }
 };
 </script>
+
+<style>
+#post__image {
+  height: 400px !important;
+}
+</style>
+
 
