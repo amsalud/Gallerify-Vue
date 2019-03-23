f9afa94a9ee9d5dd4684618b6efd273ae695c273
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2019-03-23 00:15:57 -0400

    Add cursor pointer to post

--- ui/src/components/Home.vue
+++ ui/src/components/Home.vue
@@ -1,17 +1,42 @@
 <template>
-  <v-container text-xs-center mt-3>
+  <v-container
+    text-xs-center
+    mt-3
+  >
     <v-layout row>
-      <v-dialog v-model="loading" persistent fullscreen>
+      <v-dialog
+        v-model="loading"
+        persistent
+        fullscreen
+      >
         <v-container fill-height>
-          <v-layout row justify-center align-center>
-            <v-progress-circular indeterminate :size="70" :width="7" color="secondary"></v-progress-circular>
+          <v-layout
+            row
+            justify-center
+            align-center
+          >
+            <v-progress-circular
+              indeterminate
+              :size="70"
+              :width="7"
+              color="secondary"
+            ></v-progress-circular>
           </v-layout>
         </v-container>
       </v-dialog>
     </v-layout>
     <v-flex xs12>
-      <v-carousel v-if="!loading && posts.length > 0" v-bind="{'cycle': true}" interval="3000">
-        <v-carousel-item  v-for="post in posts" :key="post._id" :src="post.imageUrl" @click.native="goToPost(post._id)">
+      <v-carousel
+        v-if="!loading && posts.length > 0"
+        v-bind="{'cycle': true}"
+        interval="3000"
+      >
+        <v-carousel-item
+          v-for="post in posts"
+          :key="post._id"
+          :src="post.imageUrl"
+          @click.native="goToPost(post._id)"
+        >
           <h1 id="carousel__title">{{post.title}}</h1>
         </v-carousel-item>
       </v-carousel>
@@ -20,20 +45,20 @@
 </template>
 
 <script>
-import { mapGetters } from 'vuex';
+import { mapGetters } from "vuex";
 export default {
-  name: 'home',
+  name: "home",
   methods: {
     handleGetCarouselPosts() {
       // reach out to Vuex store, fire action that fetches posts for carousel
-      this.$store.dispatch('getPosts');
+      this.$store.dispatch("getPosts");
     },
-    goToPost(postId){
-      this.$router.push(`/posts/${postId}`)
+    goToPost(postId) {
+      this.$router.push(`/posts/${postId}`);
     }
   },
   computed: {
-    ...mapGetters(['loading', 'posts'])
+    ...mapGetters(["loading", "posts"])
   },
   created() {
     this.handleGetCarouselPosts();
@@ -45,6 +70,7 @@ export default {
 #carousel__title {
   position: absolute;
   background-color: rgba(0, 0, 0, 0.5);
+  cursor: pointer;
   color: white;
   border-radius: 5px 5px 0 0;
   padding: 0.5m;
