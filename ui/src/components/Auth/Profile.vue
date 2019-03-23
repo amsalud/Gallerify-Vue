05fec68ff412cc6c7f241ec7186d310ab80a95af
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2019-03-22 21:43:22 -0400

    Add User details info

--- ui/src/components/Auth/Profile.vue
+++ ui/src/components/Auth/Profile.vue
@@ -1,11 +1,55 @@
 <template>
-    <v-container>
-        <h1>Profile</h1>
-    </v-container>
+
+  <v-container class="text-xs-center">
+    <!-- User Details Card -->
+    <v-flex
+      sm6
+      offset-sm3
+    >
+      <v-card
+        class="white--text"
+        color="secondary"
+      >
+        <v-layout>
+          <v-flex xs5>
+            <v-img
+              height="125px"
+              contain
+              :src="user.avatar"
+            ></v-img>
+          </v-flex>
+          <v-flex xs7>
+            <v-card-title primary-title>
+              <div>
+                <div class="headline">{{user.username}}</div>
+                <div>Joined {{user.joinDate}}</div>
+                <div class="hidden-xs-only font-weight-thin">{{user.favourites.length}}</div>
+                <div class="hidden-xs-only font-weight-thin">2 Post Added</div>
+              </div>
+            </v-card-title>
+          </v-flex>
+        </v-layout>
+      </v-card>
+    </v-flex>
+
+    <!-- Posts Liked by User -->
+    <v-layout
+      row
+      wrap
+      v-if="!userFavourites.length"
+    >
+
+    </v-layout>
+
+  </v-container>
 </template>
 
 <script>
+import { mapGetters } from "vuex";
 export default {
-  name: 'Profile'
+  name: "Profile",
+  computed: {
+    ...mapGetters(["user", "userFavourites"])
+  }
 };
 </script>
